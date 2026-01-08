263b60c6dc845ce041c10568f3e4b310979de8ed
Enoch022 <emolukola@gmail.com>
2026-01-07 19:39:42 -0600

    Add Chapter 7 examples: Dictionaries and data structures

---
+++ Chapter 7 - Dictionaries and Structuring Data/02_characterCount.py
@@ -0,0 +1,10 @@
+# Counts how many times each character appears in a message
+
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+print(count)
